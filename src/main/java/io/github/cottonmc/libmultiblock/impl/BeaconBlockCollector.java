0338d2dc97446dfbf7522c9983c32ffadfba745f
Meredith Espinosa <goclonefilms@gmail.com>
2019-09-10 18:12:53 -0700

    Revert "sketch out some beacon stuff that may or may not stay in this lib"
    This reverts commit a7d92c6819e904b44442464ab150b70de6b17ce2.

--- src/main/java/io/github/cottonmc/libmultiblock/impl/BeaconBlockCollector.java
+++
@@ -1,8 +0,0 @@
-package io.github.cottonmc.libmultiblock.impl;
-
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
-import net.minecraft.block.Block;
-
-public interface BeaconBlockCollector {
-	Object2IntMap<Block> getBeaconBlocks();
-}
